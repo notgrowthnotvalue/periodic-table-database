484d28bfd542235b25ee959312f7ae03b8c3119e
notgrowthnotvalue <notgrowthnotvalue@gmail.com>
2022-12-22 08:43:17 -0500

    fix: added comments

--- element.sh
+++ element.sh
@@ -3,8 +3,7 @@
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 
-# check if argument is provided
-# if not provided
+# If the argument is not provided
 if [[ ! $1 ]]
 then 
   echo -e "Please provide an element as an argument."
@@ -18,6 +17,7 @@ then
     MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
     BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
     TYPE=$($PSQL "SELECT type FROM properties full join types using (type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+    # using sed to remove whitespace
     echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius." | sed -r 's/\s+/ /g'
   done
 # if argument is provided and is a symbol
@@ -30,6 +30,7 @@ then
     MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
     BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
     TYPE=$($PSQL "SELECT type FROM properties full join types using (type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+    # using sed to remove whitespace
     echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius." | sed -r 's/\s+/ /g'
   done
 #if argument is provided and is a word
@@ -42,9 +43,10 @@ then
     MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
     BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
     TYPE=$($PSQL "SELECT type FROM properties full join types using (type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+    # using sed to remove whitespace
     echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius." | sed -r 's/\s+/ /g'
   done
 # if argument is provided but does not exist
-else #[[ ! $1 check if fits pattern ]]  
+else 
   echo "I could not find that element in the database."
 fi
